837cff3706d1ddb9991643cb5cd6716808af9169
Alexander Delius <alexander.delius@adacor.com>
2016-06-13 19:51:18 +0200

    first commit

---
+++ ser.php
@@ -0,0 +1,24 @@
+<?php
+
+shell_exec("stty -F /dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller_D-if00-port0 38400");
+
+$f = fopen("/dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller_D-if00-port0", "r");
+
+$buffer = "";
+$i = 10;
+while ($row = fgets($f)) {
+  if ($row && strpos($row, '$GPGGA') === 0) {
+    $buffer .= $row;
+    $i++;
+
+    if($i >= 10) {
+      $ch = curl_init($_SERVER['argv'][1]"?trackerId=1");
+      curl_setopt($ch, CURLOPT_POSTFIELDS, base64_encode(gzcompress($buffer, 9)));
+      curl_exec($ch);
+      curl_close($ch);
+      $i = 0;
+      $buffer = "";
+    }
+  }
+}
+
